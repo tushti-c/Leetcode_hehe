149be6883841d5c7bc14364b79e4cf8e9fe39326
tushti-c <tushti.tc@gmail.com>
2026-04-10 15:11:31 +1000

    first iteration of 0009 solution

--- leetcode_easy_java/0009_palindrome_number/solution.java
+++ leetcode_easy_java/0009_palindrome_number/solution.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
+        String string_x = String.valueOf(x);
+        boolean flag = true;
+        int len = string_x.length();
+        for (int i = 0; i < Math.floorDiv(len,2); i++){
+            if (string_x.charAt(i) == string_x.charAt(length - i)){
+                continue;
+            }
+            else{
+                flag = false;
+                return false;
+            }
+        }
+        if (flag == true){
+            return true;
+        }
     }
 }
