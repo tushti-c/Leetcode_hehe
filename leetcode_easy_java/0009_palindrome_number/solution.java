43676fab11a85d432d61ad232106632f02ef8d5d
tushti-c <tushti.tc@gmail.com>
2026-04-10 15:21:33 +1000

    cleaned up code and typo

--- leetcode_easy_java/0009_palindrome_number/solution.java
+++ leetcode_easy_java/0009_palindrome_number/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String string_x = String.valueOf(x);
-        boolean flag = true;
         int len = string_x.length();
         for (int i = 0; i < Math.floorDiv(len,2); i++){
-            if (string_x.charAt(i) == string_x.charAt(length - i)){
-                continue;
-            }
-            else{
-                flag = false;
+            if (string_x.charAt(i) != string_x.charAt(len - i-1)){
                 return false;
             }
         }
-        if (flag == true){
-            return true;
-        }
+        return true;
     }
 }
